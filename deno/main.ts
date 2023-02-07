12127176e74c6658fb63e4bdc4a7a8913b407495
Wralith <wralithdev@gmail.com>
2023-02-07 21:47:54 +0300

    Update deno to use oak proxy

--- deno/main.ts
+++ deno/main.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
 
 const port = Deno.env.get("PORT");
 const goServiceUrl = Deno.env.get("GO_SERVICE_URL");
@@ -9,39 +10,14 @@ if (!port || !goServiceUrl) {
 
 const posts = new Router({ prefix: "/posts" });
 
-posts.get("/", async ctx => {
-  // Do some stuff maybe
-  try {
-    const res = await fetch(`${goServiceUrl}/posts`);
-    ctx.response.body = await res.json();
-  } catch (err) {
-    console.log(err); // Handle it!
-  }
-});
-
-posts.post("/", async ctx => {
-  // ...
-  try {
-    const { title, content } = await ctx.request.body().value;
-    const res = await fetch(`${goServiceUrl}/posts`, {
-      method: "POST",
-      body: JSON.stringify({ title, content }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (res.ok) {
-      ctx.response.status = 201;
-    } else {
-      throw new Error("...");
-    }
-  } catch (err) {
-    console.log(err); // ...
-  }
-});
+posts.get("/", proxy(`${goServiceUrl}/posts`));
+posts.post("/", proxy(`${goServiceUrl}/posts`));
 
 const app = new Application();
 
 app.use(posts.routes());
+
 app.use(posts.allowedMethods());
 
+console.log(`-> http server started on port ${port}`)
 await app.listen({ port: Number(port) });
